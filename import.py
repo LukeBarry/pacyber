7e3f89cfd5e59029c26208bb7bb9b4fab992514b
Luke Barry <luke-barry@hotmail.com>
2016-10-25 13:33:13 +0000

    master

--- import.py
+++ import.py
@@ -3,18 +3,25 @@ import psycopg2
 import re
 
 tables = {
-"student_detail": "student_detail.csv",
-"course_grades" : "course_grades.csv",
-"pvaas" : "pvaas.csv",
-"pssa" : "pssa.csv",
-"keystone" : "keystone.csv",
-"first_in_math" : "first_in_math.csv",
-"study_island_english" : "study_island_english.csv",
-"study_island_math" : "study_island_math.csv",
-"benchmarks_3rd_8th" : "benchmarks_3rd_8th.csv",
-"benchmarks_alg" : "benchmarks_alg.csv",
-"benchmarks_bio" : "benchmarks_bio.csv",
-"benchmarks_lit" : "benchmarks_lit.csv",
+"student_detail": ("student_detail.csv", "PAsecure"),
+"course_grades" : ("course_grades.csv", "StateID, SchoolYear, CourseName"),
+"pvaas" : ("pvaas.csv", "PAsecureID, subject, PredictingResultsFor"),
+"pssa" : ("pssa.csv", "PAsecureID, Subject, TestedYear"),
+"keystone" : ("keystone.csv", "PAsecureID, TestName, AdminYear, AdminDate"),
+"first_in_math" : ("first_in_math.csv", "StateID"),
+"study_island_english" : ("study_island_english.csv", "SISPrimaryKey, subject, length"),
+"study_island_math" : ("study_island_math.csv", "SISPrimaryKey, subject, length"),
+"benchmarks_3rd_8th" : ("Benchmarks_3rd-8th.csv", "AlternateID, subject"),
+"benchmarks_alg" : ("Benchmarks_alg.csv", "AlternateID, subject"),
+"benchmarks_bio" : ("benchmarks_bio.csv", "AlternateID, subject"),
+"benchmarks_lit" : ("benchmarks_lit.csv", "AlternateID, course"),
+}
+
+data_types = {
+    "StateID": "bigint",
+    "AdminYear": "integer",
+    "Age": "integer",
+    # can add more here if you want them to be integers (or other types)
 }
 
 #establish connection database
@@ -23,12 +30,21 @@ conn = psycopg2.connect("")
 
 
 # loading up to read the csv files, open the files and say read this as a csv and return dictionaries
-for table, filename in tables.items():
+for table, (filename, primary_key) in tables.items():
     with open(filename) as f, conn, conn.cursor() as cur:
+        print("Processing", filename, "...")
         f = csv.DictReader(f)
-        cur.execute("select * from information_schema.columns where table_name=%s", (table,))
-        avail_cols = {row[0] for row in cur}
-        fields, cols = zip(*[(csv, sql) for csv, sql in ((fld, re.sub("[- /.]", "", fld)) for fld in f.fieldnames) if sql.lower() in avail_cols])
+        cur.execute("drop table "+table)
+        create = "create table " + table + " ("
+        cols, fields = [], []
+        for fld in f.fieldnames:
+            col = re.sub("[- /.()&,]", "", fld)
+            if col in cols or not col: continue # Hated data's gonna hate
+            if col[0].isdigit(): col = "_" + col
+            create += "%s %s not null," % (col, data_types.get(col, "text"))
+            cols.append(col); fields.append(fld)
+        create += "primary key (" + primary_key + "))"
+        cur.execute(create)
         stmt = "insert into %s (%s) values (%%s%s)" % (table, ",".join(cols), ",%s" * (len(cols)-1)) 
         for row in f: cur.execute(stmt, [row[fld] for fld in fields])
    
--- pandas_table_joins.py
+++ pandas_table_joins.py
@@ -1,10 +1,29 @@
 import pandas as pd
 
+student_detail = pd.read_csv("C:\Users\lbarry\Desktop\pandas\student_detail.csv") #db1
+course_grades = pd.read_csv("C:\Users\lbarry\Desktop\pandas\course_grades.csv") #db2
+benchmarks_3rd_8th = pd.read_csv("C:\Users\lbarry\Desktop\pandas\benchmarks_3rd-8th.csv") #db3
+benchmarks_alg = pd.read_csv("C:\Users\lbarry\Desktop\pandas\benchmarks_alg.csv") #db4
+benchmarks_bio = pd.read_csv("C:\Users\lbarry\Desktop\pandas\benchmarks_bio.csv") #db5
+benchmarks_lit = pd.read_csv("C:\Users\lbarry\Desktop\pandas\benchmarks_lit.csv") #db6
+first_in_math = pd.read_csv("C:\Users\lbarry\Desktop\pandas\first_in_math.csv") #db7
+keystone = pd.read_csv("C:\Users\lbarry\Desktop\pandas\keystone.csv") #db8
+pssa = pd.read_csv("C:\Users\lbarry\Desktop\pandas\pssa.csv") #db9
+pvaas = pd.read_csv("C:\Users\lbarry\Desktop\pandas\pvaas.csv") #db10
+study_island_english = pd.read_csv("C:\Users\lbarry\Desktop\pandas\study_island_english.csv") #db11
+study_island_math = pd.read_csv("C:\Users\lbarry\Desktop\pandas\study_island_math.csv") #db12
 
-student_detail = pd.read_csv("C:\Users\lbarry\Desktop\database\student_detail.csv")
-course_grades = pd.read_csv("C:\Users\lbarry\Desktop\database\course_grades.csv")
+db1 = student_detail
+db2 = pd.merge(db1, course_grades, how='inner', left_on='PAsecure', right_on='StateID')
+db3 = pd.merge(db2, benchmarks_3rd_8th, how='inner', left_on='PAsecure', right_on='StateID')
+db4 = pd.merge(db3, benchmarks_alg, how='inner', left_on='PAsecure', right_on='StateID')
+db5 = pd.merge(db4, benchmarks_bio, how='inner', left_on='PAsecure', right_on='StateID')
+db6 = pd.merge(db5, benchmarks_lit, how='inner', left_on='PAsecure', right_on='StateID')
+db7 = pd.merge(db6, first_in_math, how='inner', left_on='PAsecure', right_on='StateID')
+db8 = pd.merge(db7, keystone, how='inner', left_on='PAsecure', right_on='StateID')
+db9 = pd.merge(db8, pssa, how='inner', left_on='PAsecure', right_on='StateID')
+db10 = pd.merge(db9, pvaas, how='inner', left_on='PAsecure', right_on='StateID')
+db11 = pd.merge(db10, study_island_english, how='inner', left_on='PAsecure', right_on='StateID')
+db12 = pd.merge(db11, study_island_math, how='inner', left_on='PAsecure', right_on='StateID')
 
-student_detail_grades = pd.merge(student_detail, course_grades,how='inner',left_on='PAsecure',right_on='StateID')
-
-print(student_detail_grades.columns)
-
+print(db1)
